3f498023548bcaa14ba0dbedb7212b192f8b438a
juliagbmaciel <juliagabrielle863@gmail.com>
2023-11-22 14:47:19 -0300

    Add Swagger view to urls

--- backend/urls.py
+++ backend/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from core.urls import router
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +27,8 @@ urlpatterns = [
     path('api/v1/', include('core.urls')),
     path('api/v1/auth/', include('djoser.urls.authtoken')),
     path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/v1/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
 
 
